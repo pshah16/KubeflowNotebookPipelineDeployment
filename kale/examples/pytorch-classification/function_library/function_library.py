6ae444040ddbcfef61ba51fca6861d616899244f
pshah16 <pshah@equinor.com>
2020-06-01 23:58:47 +0000

    cifar notebook to pipeline example

---
+++ kale/examples/pytorch-classification/function_library/function_library.py
@@ -0,0 +1,39 @@
+import subprocess
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+if 'torch' in installed_packages:
+    import torch.nn as nn
+else:
+    subprocess.run([sys.executable, '-m', 'pip', 'install', 'torch==1.5.0', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+    
+import torch.nn.functional as F
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+    
+class Net(nn.Module):
+    def __init__(self):
+        nn.Module.__init__(self)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
---
+++ kale/examples/pytorch-classification/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='function_library',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='Prerit',
+author_email='pshah@equinor.com',
+license='MIT',
+packages=['function_library'],
+zip_safe=False)
